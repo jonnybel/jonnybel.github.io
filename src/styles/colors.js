33c41d062ad6c4e1ef7f0dcf980b18e484153ca0
Abel Almeida <jonnybel@Marias-MacBook-Air.local>
2018-10-26 23:43:43 +0200

    change photo and (matching) colours

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,9 +1,11 @@
 const colors = {
     riverbed: '#4d5b69',
     submarine: '#b8bec4',
+    darkSlateGray: '#1B3E45',
+    lightSteelBlue: '#D5DFDA'
 };
 
 export default {
-    background: colors.riverbed,
-    foreground: colors.submarine,
+    background: colors.lightSteelBlue,
+    foreground: colors.darkSlateGray,
 };
